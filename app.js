a1dbb27849c2747275e813df63bd990ce6451e84
nabi-elhraichi <nabil.elhraichi@edu.umi.ac.ma>
2021-03-24 13:31:27 +0100

    Schema

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://nabil:Wydadi1937*@cluster0.dmbgd.mongodb.net/nabil?retryWrites=true&w=majority',
+{useNewUrlParser: true,
+ useUnifiedTopology: true})
+ .then(()=> console.log('Connexion à MongoDB réussie !'))
+ .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
