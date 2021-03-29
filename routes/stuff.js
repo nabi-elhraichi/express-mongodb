296785590ad95c4fda7ab3a3c5db732bdc6f6ae7
nabi-elhraichi <nabil.elhraichi@edu.umi.ac.ma>
2021-03-29 20:57:55 +0200

    middleware authentification

--- routes/stuff.js
+++ routes/stuff.js
@@ -2,16 +2,18 @@ const express = require('express');
 
 const router = express.Router();
 
+const auth = require('../middleware/auth');
+
 const stuffCtrl = require('../controllers/stuff');
 
-router.post('/', stuffCtrl.createThing);
+router.post('/', auth ,stuffCtrl.createThing);
 
-router.put('/:id', stuffCtrl.modifyThing);
+router.put('/:id',auth ,stuffCtrl.modifyThing);
 
-router.delete('/:id', stuffCtrl.deleteThing);
+router.delete('/:id',auth ,stuffCtrl.deleteThing);
 
-router.get('/:id', stuffCtrl.getOneThing);
+router.get('/:id',auth ,stuffCtrl.getOneThing);
 
-router.get('/', stuffCtrl.getAllStuff);
+router.get('/',auth ,stuffCtrl.getAllStuff);
 
 module.exports = router;
